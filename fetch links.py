4a25b715ffadd18cc5e30f772d37bbc600a34a7c
jellyiss <63003340+jellyiss@users.noreply.github.com>
2020-04-23 15:20:17 +0200

    main script

--- fetch links.py
+++ fetch links.py
@@ -8,5 +8,25 @@ Created on Thu Apr 23 15:00:06 2020
 import urllib.request
 from bs4 import BeautifulSoup
 
+def extract(url):
+    
+    site = urllib.request.urlopen(url)
+    html = BeautifulSoup(site.read(), 'html.parser')
+    ol_anc = html.find_all('ol')
+    anc = []
+    
+    for ollist in ol_anc:
+        anc.append(ollist.findChildren())
+    return anc
 
-
+def main():
+    
+    url = input("Place your url here")
+    print( "Extracting...")
+    
+    thelist = extract(url)
+    
+    for li_anc in thelist:
+        print()
+        for nr, li in enumerate(li_list, 1):
+            print(f"{nr}. {li.text}")
