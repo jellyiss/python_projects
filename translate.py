0a574851a6b20dfd758b69054a3951701e6a381c
jellyiss <63003340+jellyiss@users.noreply.github.com>
2020-04-29 21:42:57 +0200

    implemented translate library

--- translate.py
+++ translate.py
@@ -5,3 +5,11 @@ Created on Wed Apr 29 21:22:46 2020
 @author: JellyIss
 """
 
+from googletrans import Translator
+
+def main():
+    translator = Translator()
+    result = translator.translate('Jak się masz?',dest='de')
+    print(result.text)
+    
+main()
